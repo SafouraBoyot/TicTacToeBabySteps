b067058d599c5e31a6692daa88c9d72ff3c23efe
ggsbv <ggsbv@protonmail.com>
2018-03-16 15:54:37 +0000

    TicTacToeShould.return_game_status_if_game_is_a_draw() >> failing

--- src/test/java/TicTacToeShould.java
+++ src/test/java/TicTacToeShould.java
@@ -25,4 +25,23 @@ public class TicTacToeShould {
 
         assertEquals(GameStatus.FINISHED,ticTacToe.gameStatus());
     }
+
+    @Test
+    public void
+    return_game_status_if_game_is_a_draw() throws IllegalSymbolException, IllegalPositionException {
+        Board board = new Board();
+        TicTacToe ticTacToe = new TicTacToe(board);
+
+        ticTacToe.play(0);
+        ticTacToe.play(1);
+        ticTacToe.play(2);
+        ticTacToe.play(3);
+        ticTacToe.play(4);
+        ticTacToe.play(5);
+        ticTacToe.play(7);
+        ticTacToe.play(6);
+        ticTacToe.play(8);
+
+        assertEquals(GameStatus.FINISHED,ticTacToe.gameStatus());
+    }
 }
